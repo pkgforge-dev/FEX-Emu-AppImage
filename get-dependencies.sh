861e66a03ed1b365871fb58f31748c3e3706c3ad
Link4Electronics <link4electronics@gmail.com>
2026-09-17 19:13:38 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -31,19 +31,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
-echo "Making nightly build of FEX-Emu..."
+echo "Building FEX-Emu..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/FEX-Emu/FEX"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
@@ -62,6 +50,8 @@ CC=clang CXX=clang++ cmake .. \
     -DCMAKE_CXX_COMPILER_RANLIB=/usr/bin/ranlib \
     -DCMAKE_INSTALL_PREFIX=/usr \
     -DCMAKE_BUILD_TYPE=Release \
+    -DTUNE_CPU=generic \
+    -DTUNE_ARCH=generic \
     -DUSE_LINKER=lld \
     -DENABLE_LTO=True \
     -DBUILD_TESTING=False \
